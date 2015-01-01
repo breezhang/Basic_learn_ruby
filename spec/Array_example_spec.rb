5e4769e10a460d0fa4b5877ad3bd6d81b7b3d2f8
breezang1877@hotmail.com <breezang1877@hotmail.com>
2015-01-01 22:09:58 +0800

    rspec basic usage demo + mock +stub

--- spec/Array_example_spec.rb
+++ spec/Array_example_spec.rb
@@ -13,6 +13,35 @@ describe Array do
     context 'Element Reference ' do
       subject { [1, 2, 3] }
       its([0]) { is_expected.to be 1 }
+      its([1]) { is_expected.to be 2 }
+    end
+  end
+
+  describe '#new' do
+
+    context 'Over create new array' do
+      subject { Array.new(4) { Hash.new } }
+      it { is_expected.to match_array([{}, {}, {}, {}]) }
+    end
+
+  end
+
+  describe '#combination' do
+    subject { [1, 2, 3, 4] }
+    it 'subject is instance type of Array over call method combination' do
+      expect(subject.combination(2)).to include([1, 2])
+    end
+
+    it 'mock' do
+      expect(subject).to receive(:combination).with(2)
+      subject.combination(2)
+    end
+
+    it 'stub' do
+      # allow(subject).to receive(:combination).with(2).and_return('fuck you')
+      allow(subject).to receive(:combination).with(2).and_call_original
+      # expect(subject.combination(2)).to eq([1,2])
+      expect(subject.combination(2)).to include([1, 2])
     end
   end
 
